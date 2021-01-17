35a558726d93a1d56330e09cf300d6fb6259c0ed
darthzyklus <andres.urismontilla@gmail.com>
2021-01-17 11:50:28 -0400

    format file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,11 @@
-
 import { ChakraProvider } from "@chakra-ui/react";
-import '../styles/globals.css'
-
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
-  <ChakraProvider> 
-  <Component {...pageProps} />
-  </ChakraProvider> 
-  )
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
-export default MyApp
+export default MyApp;
