6f4120f6e253bc2522165c439fc02c774f5a847f
darthzyklus <email@example.com>
2021-01-18 18:19:13 -0400

    add: bash script

---
+++ ignore-build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [[ "$VERCEL_GIT_COMMIT_REF" == "develop" || "$VERCEL_GIT_COMMIT_REF" == "main"  ]] ; then
+  # Proceed with the build
+  exit 1;
+
+else
+  # Don't build
+  exit 0;
+fi
