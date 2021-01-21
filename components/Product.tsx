bdcb2be765b2ded200fe50e50d33af978d3ead82
Lenin Nava <lenin.an.ln@gmail.com>
2021-01-21 01:16:29 -0400

    [chore] initial responsive design

--- components/Product.tsx
+++ components/Product.tsx
@@ -23,46 +23,88 @@ const ProductPage = (product: {
   description: string;
   price: number;
 }) => {
-
   const { name, image, description, price } = product;
 
   useEffect(() => {
-    setCalculatedPrice(price)
-  }, [])
+    setCalculatedPrice(price);
+  }, []);
 
   let [calculatedPrice, setCalculatedPrice] = useState(0);
   function itemChange(numberItems: string) {
-    setCalculatedPrice( price * Number(numberItems) );
+    setCalculatedPrice(price * Number(numberItems));
   }
 
+  // Optional component data ideas:
+  /* 
+    Total sold products
+    The product includes free shipping (yes, no) */
+
   return (
-    <Box>
-      <SimpleGrid columns={[2]}>
-        <Box>
-          <AspectRatio maxW="400px" ratio={4 / 3}>
-            <Image src={product.image} alt={product.name} objectFit="cover" />
-          </AspectRatio>
-        </Box>
-        <Box>
-          <Heading>{product.name}</Heading>
-          <Text>{product.description}</Text>
-          <HStack>
-            <Text>{product.price}</Text>
-            <NumberInput size="sm" defaultValue={1} onChange={itemChange} min={1} >
+    <SimpleGrid
+      h={["100%", null, "90vh"]}
+      columns={[1, 1, 2, 2]}
+      spacing="40px"
+      placeItems="center"
+      padding={5}
+    >
+      <AspectRatio
+        ratio={4 / 3}
+        w="100%"
+        minW={[null, null, "400px"]}
+        maxW={["400px", "400px", "600px"]}
+      >
+        <Image
+          src="https://bit.ly/naruto-sage"
+          alt="naruto"
+          objectFit="cover"
+        />
+      </AspectRatio>
+
+      <VStack spacing={4} align="start">
+        <Heading size="2xl">{product.name}</Heading>
+        <Text fontSize="xl" fontWeight="bold">
+          ${product.price}
+        </Text>
+        <Text fontSize="md" w={["40ch", "60ch"]} mb={10}>
+          {product.description}
+        </Text>
+        <Stack
+          w="100%"
+          direction={["column", "column", "row"]}
+          align={["left", "left", "center"]}
+          justify="start"
+        >
+          <Text>Total:</Text>
+          <Text fontSize="3xl" fontWeight="bold">
+            ${calculatedPrice}
+          </Text>
+          <Stack
+            direction="row"
+            align="center"
+            justify="center"
+            w={["100%", "100%", "auto", "auto"]}
+          >
+            <Text>Quantity:</Text>
+            <NumberInput
+              defaultValue={1}
+              onChange={itemChange}
+              min={1}
+              w={["100%", "100%", "auto", "8rem"]}
+            >
               {/* The NumberInput element needs an event listener with a function that multiplies 
               the calculated price based on its input value and updates the calculatedPrice state. */}
-              <NumberInputField />
+              <NumberInputField w={["100%", "100%", "8rem", "8rem"]} />
               <NumberInputStepper>
                 <NumberIncrementStepper />
                 <NumberDecrementStepper />
               </NumberInputStepper>
             </NumberInput>
-          </HStack>
-          <Text>Total: ${calculatedPrice}</Text>
-          <Button>Add to cart</Button>
-        </Box>
-      </SimpleGrid>
-    </Box>
+          </Stack>
+        </Stack>
+
+        <Button w={["100%", "100%", "auto"]}>Add to cart</Button>
+      </VStack>
+    </SimpleGrid>
   );
 };
 
